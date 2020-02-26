757b7220c887c00806626b1ca2f47ad95bea7e2d
Fabian Haglund <till.fabian@gmail.com>
2020-02-26 15:11:21 +0100

    config new url

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/tobofest" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/100" : "/",
     outputDir: "docs"
 }
