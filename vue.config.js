2191ac94e419e250ac34b9175f7af7ed7819d802
Fabian Haglund <till.fabian@gmail.com>
2020-02-29 14:39:56 +0100

    build for custom url

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/100" : "/",
+    //publicPath: process.env.NODE_ENV === "production" ? "/100" : "/",
     outputDir: "docs"
 }
